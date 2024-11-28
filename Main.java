4fc91d47565299f5aea55ac7e04b43fb96090c01
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2024-11-28 19:57:59 -0300

    primeiro desafio hello world em java

---
+++ Main.java
@@ -0,0 +1,14 @@
+// Your first program in any programming language is usually "Hello World!". 
+// In this first problem all you have to do is print this message on the screen.
+
+import java.io.IOException;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.println("Hello World!");
+ 
+    }
+ 
+}
