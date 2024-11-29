19ea99bfeebf76e6097c1d9b0770431aaee621a2
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2024-11-28 21:00:00 -0300

    exercicio 1005, uma media ponderada entre dois alunos

--- Ex1005.java
+++ Ex1005.java
@@ -1,24 +1,18 @@
-// Read two integer values. After this, calculate the product between them and store the result in a variable named PROD. 
-// Print the result like the example below.
-//  Do not forget to print the end of line after the result, otherwise you will receive “Presentation Error”.
-
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Ex1005 {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
 
-  public static void main(String[] args) throws IOException {
-    Scanner scanner = new Scanner(System.in);
+      
+      double A = scanner.nextDouble();
+      double B = scanner.nextDouble();
 
-    int A = scanner.nextInt();
-    int B = scanner.nextInt();
+      double MEDIA = (A * 3.5 + B * 7.5) / (3.5 + 7.5);
 
-    int PROD = A * B;
+      
+      System.out.printf("MEDIA = %.5f\n", MEDIA);
 
-    System.out.println("PROD = " + PROD);
-
-    scanner.close();
-
-  }
-
+      scanner.close();
+    }
 }
